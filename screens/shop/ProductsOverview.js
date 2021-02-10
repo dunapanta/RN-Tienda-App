10c6a5232cbc32590383cf0553e24d8e979f1a1e
Daniel Unapanta <dunapanta2@gmail.com>
2021-02-10 09:49:38 -0600

    Adding model for product

---
+++ screens/shop/ProductsOverview.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FlatList, View, Text, StyleSheet }  from 'react-native'
+
+const ProductsOverview = () => {
+    return (
+        <FlatList>
+
+        </FlatList>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ProductsOverview
