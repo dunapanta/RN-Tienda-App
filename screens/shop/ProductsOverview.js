59a6433f14ab06e256e39b173bf7330929fe0a8c
Daniel Unapanta <dunapanta2@gmail.com>
2021-02-10 10:39:42 -0600

    Adding FlatList to ProductsOverview

--- screens/shop/ProductsOverview.js
+++ screens/shop/ProductsOverview.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { FlatList, View, Text, StyleSheet }  from 'react-native'
+import { useSelector } from 'react-redux'
 
 const ProductsOverview = () => {
-    return (
-        <FlatList>
 
-        </FlatList>
+    const products = useSelector( state => state.products.availableProducts)
+
+    return (
+        <FlatList 
+            //keyExtractor={item => item.id} 
+            data={products} 
+            renderItem={ itemData => <Text>{itemData.item.title}</Text>}
+        />
     )
 }
 
