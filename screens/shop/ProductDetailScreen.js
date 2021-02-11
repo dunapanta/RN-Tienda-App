1e41571aa489d04f84db725d0a43324f5c1b09f6
Daniel Unapanta <dunapanta2@gmail.com>
2021-02-10 18:39:49 -0600

    Adding content to ProductDetailScreen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { View, Text, Image, Button, ScrrollView, StyleSheet} from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import { useSelector } from 'react-redux'
+
+import Colors from '../../constants/Colors'
 
 const ProductDetailScreen = ({ navigation }) => {
 
@@ -9,9 +12,14 @@ const ProductDetailScreen = ({ navigation }) => {
         state => state.products.availableProducts.find(p => p.id === productId) )
 
     return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: selectedProduct.imageUrl}} />
+            <View style={styles.action}>
+                <Button title="Agregar al Carrito" onPress={ () => {}} color={Colors.secondary}/>
+            </View>
+            <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     )
 }
 
@@ -22,7 +30,25 @@ ProductDetailScreen.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: '100%',
+        height: 300
+    },
+    action: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: Colors.secondaryDark,
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
+    },
 })
 
 export default ProductDetailScreen
